3c3bfb7e92974b32f5b183de55ffd9cc7a36bc91
stickynote <thlbgraves@gmail.com>
2022-12-31 21:30:21 -0600

    Happy new years! halfway in between function writting

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+}
