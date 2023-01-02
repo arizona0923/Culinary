996baaadb89d44e5968b52191bc7f6543eedbe71
stickynote <thlbgraves@gmail.com>
2023-01-01 21:33:45 -0600

    responsive for small screens 600px. medium screens and large screens

--- index.js
+++ index.js
@@ -15,4 +15,8 @@ function opentab(tabname) {
 var sidemenu = document.getElementById("sidemenu");
 
 function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
 }
